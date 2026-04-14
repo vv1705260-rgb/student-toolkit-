2692fb9200a29ed6ed3a9f1b11e98c688ec223b4
vv1705260-rgb <vv1705260@gmail.com>
2026-04-14 16:49:54 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -20,3 +20,33 @@ function startTimer() {
         if(time < 0) clearInterval(interval);
     }, 1000);
 }
+// Load saved tasks
+window.onload = function() {
+    let saved = JSON.parse(localStorage.getItem("tasks")) || [];
+    saved.forEach(task => showTask(task));
+};
+
+function addTask() {
+    let task = document.getElementById("task").value;
+
+    if(task === "") return;
+
+    showTask(task);
+
+    let saved = JSON.parse(localStorage.getItem("tasks")) || [];
+    saved.push(task);
+    localStorage.setItem("tasks", JSON.stringify(saved));
+
+    document.getElementById("task").value = "";
+}
+
+function showTask(task) {
+    let li = document.createElement("li");
+    li.innerText = task;
+
+    li.onclick = function() {
+        li.remove();
+    };
+
+    document.getElementById("list").appendChild(li);
+}
